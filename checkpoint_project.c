ae9feec6bc43dd2876396caab3aeb71e65836846
paz.menach <paz.menach@gmail.com>
2024-09-10 14:16:01 +0300

    Finish feature

--- checkpoint_project.c
+++ checkpoint_project.c
@@ -26,31 +26,46 @@ static LIST_HEAD(blocked_ip_list);
 unsigned int block_ip_hook(void *priv, struct sk_buff *skb, const struct nf_hook_state *state);
 
 //
-//  HOOK SET UP
+//  HOOK FUNCTION
 //
 
 // Netfilter hook
 static struct nf_hook_ops pre_routing_hook;
 
-// Hook function
+// Hook main function
 unsigned int block_ip_hook(void *priv,
                            struct sk_buff *skb,
                            const struct nf_hook_state *state)
 {
     struct iphdr *ip_header;
-    
+    struct blocked_ip *entry;
 
     if (!skb)
         return NF_ACCEPT;
 
-    ip_header = ip_hdr(skb);  // Get the IP header
+    ip_header = ip_hdr(skb);
 
-    if (ip_header && ip_header->saddr == in_aton(BLOCKED_IP)) {
-        printk(KERN_INFO "Blocked packet from %pI4\n", &ip_header->saddr);
-        return NF_DROP;
+    if (!ip_header)
+        return NF_ACCEPT;
+
+    // Iterate
+    list_for_each_entry(entry, &blocked_ip_list, list) {
+        if (ip_header->saddr == entry->ip) {
+            printk(KERN_INFO "Blocked packet from %pI4\n", &ip_header->saddr);
+            return NF_DROP;
+        }
     }
 
     return NF_ACCEPT;
+}
+
+void free_blocked_ips(void) {
+    struct blocked_ip *entry, *tmp;
+
+    list_for_each_entry_safe(entry, tmp, &blocked_ip_list, list) {
+        list_del(&entry->list);
+        kfree(entry);
+    }
 }
 
 void add_blocked_ip(const char *ip_str) {
@@ -62,15 +77,21 @@ void add_blocked_ip(const char *ip_str) {
     new_node->ip = in_aton(ip_str);
 
     INIT_LIST_HEAD(&new_node->list);
-    list_add(&new_node>list, &blocked_ip_list);
+    list_add(&new_node->list, &blocked_ip_list);
 }
 
-void hook_init(){
+void hook_init(void){
     // Fill hook struct fields.
     pre_routing_hook.hook = block_ip_hook; // Hook function
     pre_routing_hook.hooknum = NF_INET_PRE_ROUTING; // Stage
     pre_routing_hook.pf = PF_INET; // protocol (IPV4)
     pre_routing_hook.priority = NF_IP_PRI_FIRST;
+}
+
+void add_example(void){
+    add_blocked_ip("13.226.2.101"); // TikTok
+    add_blocked_ip("13.226.2.6");
+    add_blocked_ip("13.226.2.94");
 }
 
 //
@@ -83,12 +104,14 @@ void hook_init(){
 //
 
 static int __init checkpoint_init(void) {
-    printk(KERN_INFO "Kernel project started\n");
+    printk(KERN_INFO "IP BLOCKER LUNCHED\n");
     
     hook_init();
 
     // Register the hook
     nf_register_net_hook(&init_net, &pre_routing_hook);
+
+    add_example();
 
     return 0;
 }
@@ -98,6 +121,7 @@ static void __exit checkpoint_exit(void) {
 
     // Unregister the Netfilter hook
     nf_unregister_net_hook(&init_net, &pre_routing_hook);
+    free_blocked_ips();
 }
 
 //
